506c3ce26a09bff0cf968df432260782f6d4a7bf
Lucas-Manoel <119638820+Lucas-Manoel@users.noreply.github.com>
2023-06-22 21:28:29 -0300

    terminei o praticando-2 e adicionei uma foto

--- criando-primeiro-exer/praticando-2/script.js
+++ criando-primeiro-exer/praticando-2/script.js
@@ -8,16 +8,42 @@ function verificar(){
 }else{
     var fsex = document.getElementsByName('radsex')
     var idade = ano - Number(fano.value)
-    var genero =''
+    var genero = ''
     var img =document.createElement('img')
     img.setAttribute('id' , 'foto')
     if (fsex [0].checked){
         genero = 'Homem'
         if (idade >=0 && idade < 10){
             //criança
-            img.setAttribute()
-        }
+            img.setAttribute('src','imagens/criança.png')
+            }else if (idade < 21){
+                //jovem
+                img.setAttribute('src','imagens/jovem.png')
+            }else if (idade <50) {
+                //adulto
+                img.setAttribute('src','imagens/adulto.png')
+            }else {
+                //idoso
+                img.setAttribute('src','imagens/idoso.png')
+            }
+    } else if (fsex[1].checked) {
+        genero = 'Mulher'
+        if (idade >=0 && idade < 10){
+            //criança
+            img.setAttribute('src','imagens/criança.pgn')
+            }else if (idade < 21){
+                //jovem
+                img.setAttribute('src','imagens/jovem-mulher.png')
+            }else if (idade <50) {
+                //adulto
+                img.setAttribute('src','imagens/adulta.png')
+            }else {
+                //idoso
+                img.setAttribute('src','imagens/idosa.png')
+            }
     }
+    res.innerHTML =`Detectamos ${genero} com ${idade} anos`
+    res.appendChild(img)
 }
 
 }
