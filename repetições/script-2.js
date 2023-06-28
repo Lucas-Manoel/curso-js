7aed231497b862bbbf24390608af68bf485377e3
Lucas-Manoel <119638820+Lucas-Manoel@users.noreply.github.com>
2023-06-27 22:55:03 -0300

    exercicio 2 tabuada

--- repetições/script-2.js
+++ repetições/script-2.js
@@ -1,3 +1,20 @@
 function tabuada(){
-    
+     let num =document.getElementById('txtn')
+     let tab =document.getElementById('seltab')
+
+    if (num.value.lenght ==0){
+         window.alert('por favor digite outro numero')
+    } else{
+        let n= Number(num.value)
+        let c =1
+        tab.innerHTML = ""
+        while (c<= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c}= ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+
+    }
 }
